256b238d26c28a10e9f256369d3ee6e20a412f4c
Shivansh Dubey <114563932+webwizzard07@users.noreply.github.com>
2024-09-04 21:06:40 +0530

    video 57 completed

---
+++ js/video57/index.js
@@ -0,0 +1,42 @@
+console.log("this is loop video")
+
+// let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+    
+// }
+
+// let obj = {
+//     name:"shivansh",
+//     age:19,
+//     company:"tata"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key) 
+
+// }
+
+
+// for (const c of "shivansh") {
+//     console.log(c)    
+// }
+
+// let a = 5;
+// while (a<6) {
+//     console.log(a)
+//     a++ ;
+// }
+
+let a = 10;
+do {
+    console.log(a)
+    a++;
+} while (a < 6);
+
